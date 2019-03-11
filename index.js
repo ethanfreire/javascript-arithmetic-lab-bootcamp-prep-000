398c4286dac60a8525d059f2476ef137a43ebf35
Ethan Freire <element55star@gmail.com>
2019-03-11 12:13:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-function add(){
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function add(a,b){
   a + b
 }
-function subtract(){
+function subtract(a,b){
   a - b
 }
-function multiply(){
+function multiply(a,b){
   a * b
 }
-function divide (){
+function divide (a,b){
   a / b
 }
 function inc(n){
